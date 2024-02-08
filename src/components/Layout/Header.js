f741e8682b7e6b2431769765e9b32e068f23137b
Abdulajishs <abdulajeeshsb@gmail.com>
2024-02-08 12:29:02 +0530

    Add Restaurant Summary

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import mealsImage from "../../assets/meals.jpg"
-import classes from "./Header.module.css"
 import HeaderCardButton from "./HeaderCardButton";
+import classes from "./Header.module.css"
 
 const Header = () => {
     return (
--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -3,17 +3,17 @@ import CartIcon from "../Card/CardIcon";
 
 import classes from "./HeaderCardButton.module.css"
 
-const HeaderCardButton = (props) =>{
-    return(
-            <button className={classes.button}> 
-                <span className={classes.icon}>
-                    <CartIcon/>
-                </span>
-                <span>Your Cart</span>
-                <span className={classes.badge}>
-                    3
-                </span>
-            </button>
+const HeaderCardButton = (props) => {
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>
+                3
+            </span>
+        </button>
     )
 }
 
